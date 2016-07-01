2fe6ba7eff1cf3de5701282274ec6313016c9437
duanshuaimin <duanshuaimin@hotmail.com>
2016-07-01 13:42:08 +0800

    some tools

---
+++ stopPrestoCluster.py
@@ -0,0 +1,11 @@
+from paramiko.client import SSHClient
+from paramiko.client import AutoAddPolicy
+client=SSHClient()
+autoAddpolicy=AutoAddPolicy()
+client.set_missing_host_key_policy(autoAddpolicy)
+client.load_system_host_keys()
+hosts=['192.168.100.5','192.168.100.2','192.168.100.3','192.168.100.4','192.168.100.6']
+for host in hosts:
+	client.connect(host,port=22,username='root',password='Luotuocao2008',look_for_keys=False, allow_agent=False)
+	
+	stdin, stdout, stderr = client.exec_command('launcher stop')
