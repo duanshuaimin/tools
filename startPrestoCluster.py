607ce72df61b4e03b47be9e17eb101dbbfa3f57e
duanshuaimin <duanshuaimin@hotmail.com>
2016-07-01 13:56:36 +0800

    fix some words

--- startPrestoCluster.py
+++ startPrestoCluster.py
@@ -6,6 +6,6 @@ client.set_missing_host_key_policy(autoAddpolicy)
 client.load_system_host_keys()
 hosts=['192.168.100.5','192.168.100.2','192.168.100.3','192.168.100.4','192.168.100.6']
 for host in hosts:
-	client.connect(host,port=22,username='root',password='Luotuocao2008',look_for_keys=False, allow_agent=False)
+	client.connect(host,port=22,username='test',password='2222',look_for_keys=False, allow_agent=False)
 	
 	stdin, stdout, stderr = client.exec_command('launcher start')
